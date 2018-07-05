62b1e68f78d576399b7c074d687bc98ce38162e4
lhl <lihaoliang2006@126.com>
2018-07-05 10:34:28 +0800

    first commit

---
+++ js/watcher.js
@@ -0,0 +1,50 @@
+
+
+function Watcher(vm, exp, cb) {
+  this.cb = cb;
+  this.vm = vm;
+  this.exp = exp;
+  this.depIds = {};
+  if (typeof exp === 'function') {
+    this.getter = exp;
+  } else {
+    this.getter = this.parseGetter(exp);
+  }
+  this.value = this.get();
+}
+Watcher.prototype = {
+  update: function () {
+    this.run();
+  },
+  run: function () {
+    var value = this.get();
+    var oldValue = this.value;
+    if (value !== oldValue) {
+      this.value = value;
+      this.cb.call(this.vm, value, oldValue);
+    }
+  },
+  addDep: function (dep) {
+    if (!this.depIds.hasOwnProperty(dep.id)) {
+      dep.addSub(this);
+      this.depIds[dep.id] = dep;
+    }
+  },
+  get: function () {
+    Dep.target = this;
+    var value = this.getter.call(this.vm, this.vm);
+    Dep.target = null;
+    return value;
+  },
+  parseGetter: function (exp) {
+    if (/[^\w.$]/.test(exp)) return;
+    var exps = exp.split('.');
+    return function (obj) {
+      for (var i = 0, len = exps.length; i < len; i++){
+        if(!obj) return;
+        obj = obj[exps[i]];
+      }
+      return obj;
+    }
+  }
+}
